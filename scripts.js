94e5c04e26742b5e395918cf07c57bbef77a5f47
Brunogitguimaraes <brunodasilvaguimaraes609@gmail.com>
2023-12-26 15:33:46 -0300

    Novas alterações em JavaScript

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const convertButton = document.querySelector(".convert-button");
-const currencySelect = document.querySelector(".currency-select");
+const currencySelect = document.querySelector(".convert-other-currencies");
 
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
@@ -10,12 +10,19 @@ function convertValues() {
 
   const dolarToday = 4.95;
   const euroToday = 5.34;
+  const libraToday = 6.13;
+  const bitcoinToday = 201.805;
+
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputCurrencyValue);
 
   if (currencySelect.value == "dolar") {
     currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
-    }).format(inputCurrencyValue / euroToday);
+    }).format(inputCurrencyValue / dolarToday);
   }
 
   if (currencySelect.value == "euro") {
@@ -24,11 +31,20 @@ function convertValues() {
       currency: "EUR",
     }).format(inputCurrencyValue / euroToday);
   }
-  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  }).format(inputCurrencyValue);
+  if (currencySelect.value == "libra") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency: "GBP",
+    }).format(inputCurrencyValue / libraToday);
+  }
+  if (currencySelect.value == "bitcoin") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "BTC",
+    }).format(inputCurrencyValue / bitcoinToday);
+  }
 }
+
 function changeCurrency() {
   const currencyName = document.getElementById("currency-name");
   const currencyImage = document.querySelector(".currency-img");
@@ -42,6 +58,15 @@ function changeCurrency() {
     currencyName.innerHTML = "Euro";
     currencyImage.src = "./assets/Euro.png";
   }
+  if (currencySelect.value == "libra") {
+    currencyName.innerHTML = "Libra";
+    currencyImage.src = "./assets/libra.png";
+  }
+  if (currencySelect.value == "bitcoin") {
+    currencyName.innerHTML = "Bitcoin";
+    currencyImage.src = "./assets/bitcoin.png";
+  }
+
   convertValues();
 }
 currencySelect.addEventListener("change", changeCurrency);
