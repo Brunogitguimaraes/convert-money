af8fc231fa1c4959a4c5fe2c58dffe12dff18ec9
Brunogitguimaraes <brunodasilvaguimaraes609@gmail.com>
2023-12-28 11:18:52 -0300

    Adicionado novas funções

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".convert-other-currencies");
+const currencySelectConvert = document.querySelector(".convert-currency");
+const currencyConversionAudio = document.querySelector(
+  ".currency-convert-audio"
+);
 
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
@@ -8,16 +12,33 @@ function convertValues() {
 
   console.log(currencySelect.value);
 
-  const dolarToday = 4.95;
+  const dolarToday = 4.85;
   const euroToday = 5.34;
   const libraToday = 6.13;
-  const bitcoinToday = 201.805;
+  const bitcoinToday = 208495.97;
+
+  const dolarToEuro = 0.9;
 
   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
   }).format(inputCurrencyValue);
 
+  if (currencySelectConvert.value == "dolar-tow") {
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrencyValue);
+  }
+
+  //Conversão a partir do real
+
+  if (currencySelect.value == "real") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(inputCurrencyValue);
+  }
   if (currencySelect.value == "dolar") {
     currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
       style: "currency",
@@ -41,14 +62,49 @@ function convertValues() {
     currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
       style: "currency",
       currency: "BTC",
+      minimumSignificantDigits: 2,
+      maximumSignificantDigits: 6,
     }).format(inputCurrencyValue / bitcoinToday);
   }
+  //Conversão a partir do Dólar Americano
+  if (
+    currencySelectConvert.value == "dolar-two" &&
+    currencySelect.value == "dolar"
+  ) {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrencyValue);
+  }
+  if (
+    currencySelectConvert.value == "dolar-two" &&
+    currencySelect.value == "real"
+  ) {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(inputCurrencyValue * dolarToday);
+  }
+  if (
+    currencySelectConvert.value == "dolar-two" &&
+    currencySelect.value == "euro"
+  ) {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputCurrencyValue * dolarToEuro);
+  }
+  currencyConversionAudio.play();
 }
 
 function changeCurrency() {
   const currencyName = document.getElementById("currency-name");
   const currencyImage = document.querySelector(".currency-img");
 
+  if (currencySelect.value == "real") {
+    currencyName.innerHTML = "Real";
+    currencyImage.src = "./assets/Real.png";
+  }
   if (currencySelect.value == "dolar") {
     currencyName.innerHTML = "Dólar americano";
     currencyImage.src = "./assets/USA.png";
@@ -66,8 +122,23 @@ function changeCurrency() {
     currencyName.innerHTML = "Bitcoin";
     currencyImage.src = "./assets/bitcoin.png";
   }
-
   convertValues();
 }
+function changeValuesConvert() {
+  const currencyNameValue = document.getElementById("currency-value-convert");
+  const currencyImageConvert = document.querySelector(".currency-image");
+
+  if (currencySelectConvert.value == "real-two") {
+    currencyNameValue.innerHTML = "Real";
+    currencyImageConvert.src = "./assets/Real.png";
+  }
+  if (currencySelectConvert.value == "dolar-two") {
+    currencyNameValue.innerHTML = "Dólar americano";
+    currencyImageConvert.src = "./assets/USA.png";
+  }
+  convertValues();
+}
+
 currencySelect.addEventListener("change", changeCurrency);
+currencySelectConvert.addEventListener("change", changeValuesConvert);
 convertButton.addEventListener("click", convertValues);
